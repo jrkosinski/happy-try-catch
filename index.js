4162ae6b099eeee24726f00a0317090214e76594
Acer <john.kosinski@gmail.com>
2018-07-14 15:22:12 +0700

    removed old module.exports; added comment

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 /**
  * Handler 
+ * Exception handler with try/catch wrapping a given functional expression. 
  * 
  * John R. Kosinski 
  * 11 May 2016
@@ -147,41 +148,4 @@ function Handler(options) {
 
 module.exports = {
     create
-};
-
-module.exports = function excepUtil(logPrefix) {
-    function ExcepUtil() {
-        const _this = this; 
-
-        this.try = (expr, options) => {
-            let error = null; 
-            try {
-                return expr();
-            }
-            catch(err) {
-                try {
-                    error = err;
-                    _this.handleError(err); 
-                    if (options && options.onError) {
-                        return options.onError(err); 
-                    }
-                    if (options && options.rethrow) {
-                        throw err; 
-                    }
-
-                    return options ? options.defaultValue : null; 
-                } catch(e) {}
-            }
-            finally {
-                if (options && options.finally) 
-                    return options.finally(error); 
-            }
-        }; 
-
-        this.handleError = (err) => {
-            console.log(logPrefix + ': ' + err); 
-        }; 
-    }
-
-    return new ExcepUtil();
 };
